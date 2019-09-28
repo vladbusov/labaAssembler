5d069b94cdf4c5688c815c7018d39534910aeee0
vladbusov <to2002to2002@gmail.com>
2019-09-28 19:17:58 +0300

    first commit

---
+++ lab-1-config.s
@@ -0,0 +1,8 @@
+; Константы
+
+STACK_TOP	EQU	0x20000100		; Вершина стека
+DELAY_VAL_MAX	EQU	1520		; Максимум сверху 80
+DELAY_VAL_MIN	EQU 20			; Минимум снизу 20
+DELAY_T		EQU	1540			; Период 100
+DELAY_SUB	EQU	10
+	END
